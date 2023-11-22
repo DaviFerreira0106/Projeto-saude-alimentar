484854b80916585cf4e526df0a7736f34e32c96b
Davi Ferreira <daviferreira0106@gmail.com>
2023-11-22 14:29:02 -0300

    atualização da pasta view

---
+++ applife/lib/view/services/auth_or_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthOrHomePage extends StatefulWidget {
+  const AuthOrHomePage({super.key});
+
+  @override
+  State<AuthOrHomePage> createState() => _AuthOrHomePageState();
+}
+
+class _AuthOrHomePageState extends State<AuthOrHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
